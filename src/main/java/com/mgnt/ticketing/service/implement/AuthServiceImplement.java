d1a8c52e11ef3dfc1e20d36f3380d424c69246b2
hyeseong-dev <hyeseong43@gmail.com>
2024-06-21 20:46:08 +0900

    feat: UPDATE AuthServiceImplement 업데이트

--- src/main/java/com/mgnt/ticketing/service/implement/AuthServiceImplement.java
+++ src/main/java/com/mgnt/ticketing/service/implement/AuthServiceImplement.java
@@ -3,6 +3,8 @@ package com.mgnt.ticketing.service.implement;
 import com.mgnt.ticketing.dto.ResponseDto;
 import com.mgnt.ticketing.dto.request.auth.SignUpRequestDto;
 import com.mgnt.ticketing.dto.request.auth.UserRegisteredEvent;
+import com.mgnt.ticketing.dto.response.ResponseCode;
+import com.mgnt.ticketing.dto.response.ResponseMessage;
 import com.mgnt.ticketing.dto.response.auth.SignUpResponseDto;
 import com.mgnt.ticketing.dto.response.auth.TokenReqRes;
 import com.mgnt.ticketing.entity.UserEntity;
@@ -34,12 +36,18 @@ public class AuthServiceImplement implements AuthService {
     private final ApplicationEventPublisher eventPublisher;
 
 
+    @Override
     public ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto) {
-
         try {
-
             boolean hasEmail = userRepository.existsByEmail(dto.getEmail());
-            if (hasEmail) return SignUpResponseDto.duplicateEmail();
+            if (hasEmail) {
+                return ResponseEntity
+                        .status(409)
+                        .body(SignUpResponseDto.builder()
+                                .code(ResponseCode.DUPLICATED_EMAIL)
+                                .message(ResponseMessage.DUPLICATED_EMAIL)
+                                .build());
+            }
 
             UserEntity userEntity = UserEntity.from(dto, passwordEncoder.encode(dto.getPassword()));
             userRepository.save(userEntity);
@@ -47,14 +55,19 @@ public class AuthServiceImplement implements AuthService {
             // 이벤트 발행
             eventPublisher.publishEvent(new UserRegisteredEvent(userEntity.getEmail(), userEntity.getName()));
 
-        } catch(Exception exception) {
-            exception.printStackTrace();
-            log.error(exception.getMessage());
-            return ResponseDto.databaseError();
+            return ResponseEntity.ok(SignUpResponseDto.builder()
+                    .code(ResponseCode.SUCCESS)
+                    .message(ResponseMessage.SIGN_UP_SUCCESS)
+                    .build());
+        } catch (Exception exception) {
+            log.error("Database error: {}", exception.getMessage(), exception);
+            return ResponseEntity
+                    .status(500)
+                    .body(SignUpResponseDto.builder()
+                            .code(ResponseCode.DATABASE_ERROR)
+                            .message(ResponseMessage.DATABASE_ERROR)
+                            .build());
         }
-
-        return SignUpResponseDto.success();
-
     }
 
 //    public TokenReqRes login(TokenReqRes request) {
