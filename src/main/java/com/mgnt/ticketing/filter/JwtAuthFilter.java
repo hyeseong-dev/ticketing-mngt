9defcec2b7507258232f66ca8306188c2cb8f884
hyeseong-dev <hyeseong43@gmail.com>
2024-06-24 10:08:12 +0900

    debug: securityFilterChain에 허용된 URL을 matcher에 등록하였지만 필터에서 다시 진입하는 이슈로 인해 추가로 허용되는 URL은 인증 필터 로직을 받지 않도록 분기 처리함.

--- src/main/java/com/mgnt/ticketing/filter/JwtAuthFilter.java
+++ src/main/java/com/mgnt/ticketing/filter/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package com.mgnt.ticketing.filter;
 
+import com.mgnt.ticketing.config.SecurityProperties;
 import com.mgnt.ticketing.security.JwtUtil;
 import com.mgnt.ticketing.security.UserDetailServiceImpl;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -20,6 +21,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -28,41 +30,53 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final UserDetailServiceImpl userDetailServiceImpl;
+    private final SecurityProperties securityProperties;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader(JwtUtil.AUTHORIZATION_HEADER);
-        final String jwtToken;
-        String userEmail;
-
-        if (authHeader == null || authHeader.isBlank()) {
+        // 허용된 URI는 인증 기능 로직을 받지 않도록 함.
+        if (isAllowedUri(request.getRequestURI())) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        jwtToken = authHeader.substring(JwtUtil.BEARER_PREFIX.length());
+        final String jwtToken = getJwtToken(request);
+
         try {
-            userEmail = jwtUtil.extractUsername(jwtToken);
-
+            final String userEmail = jwtUtil.extractUsername(jwtToken);
             if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = userDetailServiceImpl.loadUserByUsername(userEmail);
-
-                if (jwtUtil.isTokenValid(jwtToken, userDetails)) {
-                    UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, userDetails.getAuthorities()
-                    );
-
-                    token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(token);
-                }
+                authenticateUser(request, userEmail, jwtToken);
             }
-        } catch (ExpiredJwtException | MalformedJwtException | SignatureException | UnsupportedJwtException |
-                 IllegalArgumentException e) {
+        } catch (ExpiredJwtException | MalformedJwtException | SignatureException | UnsupportedJwtException | IllegalArgumentException e) {
             log.error(e.getMessage());
             request.setAttribute("exception", e);
         }
 
         filterChain.doFilter(request, response);
     }
-}
 
+    private boolean isAllowedUri(String requestUri) {
+        List<String> allowedUris = securityProperties.getAllowedUris();
+        return allowedUris.stream().anyMatch(requestUri::startsWith);
+    }
+
+    private String getJwtToken(HttpServletRequest request) {
+        final String authHeader = request.getHeader(JwtUtil.AUTHORIZATION_HEADER);
+        if (authHeader != null && authHeader.startsWith(JwtUtil.BEARER_PREFIX)) {
+            return authHeader.substring(JwtUtil.BEARER_PREFIX.length());
+        }
+        return null;
+    }
+
+    private void authenticateUser(HttpServletRequest request, String userEmail, String jwtToken) {
+        UserDetails userDetails = userDetailServiceImpl.loadUserByUsername(userEmail);
+        if (jwtUtil.isTokenValid(jwtToken, userDetails)) {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities()
+            );
+            token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(token);
+        }
+    }
+
+}
