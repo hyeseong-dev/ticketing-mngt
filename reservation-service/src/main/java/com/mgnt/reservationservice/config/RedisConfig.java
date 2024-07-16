f1bc951309b2c1400a6dfcae3b1227b35356fa6f
hyeseong-dev <hyeseong43@gmail.com>
2024-07-16 11:12:01 +0900

    debug: 대기열 저장 시, 직렬화, 역직렬하 오류로 인함. - 기존 코드는 문자열 저장 시, 따옴표가 저장되고 더불어서 조회시에 따옴표까지 같이 입력 값으로 넣지 않으면 오류가 나타났다. 이러한 불편함을 해소하고자 설정 변경

--- reservation-service/src/main/java/com/mgnt/reservationservice/config/RedisConfig.java
+++ reservation-service/src/main/java/com/mgnt/reservationservice/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.mgnt.reservationservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,11 +12,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        // ObjectMapper 설정
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+
+        // Jackson2JsonRedisSerializer 생성자를 통해 ObjectMapper 설정
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+
+        // key는 String으로, value는 JSON으로 직렬화
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+
         return template;
     }
 }
