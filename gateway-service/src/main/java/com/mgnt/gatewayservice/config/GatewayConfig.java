cee6f9051cecdbf14e857652133de7a1440a33e4
hyeseong-dev <hyeseong43@gmail.com>
2024-07-18 04:56:34 +0900

    deubg: api-gateway 오류 1. http request header에 새로운 헤더 키를 넣고 GET 요청 시, 200응답은 하지만 resposne body가 비어서 응답하는 오류 2. 이후 로직 변경 이후, http POST request 시도를 할 경우 다른 서비스로 요청이 전달 되지 않는 이슈

--- gateway-service/src/main/java/com/mgnt/gatewayservice/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/mgnt/gatewayservice/config/GatewayConfig.java
@@ -45,6 +45,7 @@ public class GatewayConfig {
                         .uri("lb://PAYMENT-SERVICE"))
                 .route("concert-service", r -> r
                         .path("/api/concerts/**")
+                        .filters(f -> f.filter(authFilter.apply(new AuthorizationHeaderFilter.Config())))
                         .uri("lb://CONCERT-SERVICE"))
                 .build();
     }
